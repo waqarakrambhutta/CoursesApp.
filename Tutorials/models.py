5e1f67de87f29b952e9a59bc7998ad7a2380a02c
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-06-30 23:15:30 +0500

    users settings

--- Tutorials/models.py
+++ Tutorials/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Author(models.Model):
+    phone = models.CharField(max_length=255)
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
