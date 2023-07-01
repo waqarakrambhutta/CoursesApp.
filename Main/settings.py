2774d5285439c1cb19dae43f643f4b32860507ae
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-07-01 12:06:24 +0500

    setting up admin site.

--- Main/settings.py
+++ Main/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'Main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'w3schools',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'waqar123!@#'
     }
 }
 
--- Tutorials/admin.py
+++ Tutorials/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Author
 
 # Register your models here.
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['phone','user']
+    search_fields = ['user']
+    
